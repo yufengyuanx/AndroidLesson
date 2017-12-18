cb0e6a024c25e8a95901e96e8a8e07cf4d752be0
Yufeng Yuan <Yufeng_Yuan@student.uml.edu>
2017-12-18 01:05:00 -0800

    create lesson 2

---
+++ AndroidLesson2/app/src/main/java/com/yufengyuanx/androidlesson2/DataService.java
@@ -0,0 +1,28 @@
+package com.yufengyuanx.androidlesson2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yufengyuan on 12/17/17.
+ */
+
+public class DataService {
+
+    /**
+     * Fake all the event data for now.
+     */
+
+    public static List<Event> getEventData() {
+        List<Event> eventData = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            eventData.add(
+                    new Event("Event title",
+                              "1111 dr, fake city, CA, 11111",
+                               "This is description part")
+            );
+        }
+        return eventData;
+    }
+
+}
---
+++ AndroidLesson2/app/src/main/java/com/yufengyuanx/androidlesson2/EventFragment.java
@@ -0,0 +1,36 @@
+package com.yufengyuanx.androidlesson2;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventFragment extends Fragment {
+
+
+    public EventFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.event_list);
+        listView.setAdapter(new EventAdapter(getActivity()));
+        return view;
+
+
+
+    }
+
+}
